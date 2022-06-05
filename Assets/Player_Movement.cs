599cc93eaeb91b14b8e5845ec1ff71b45b0d2894
NachoBerardo <nachoberardo2005@gmail.com>
2022-06-05 16:45:47 -0300

    Placas
    Placas que no andan

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_Movement : MonoBehaviour
 {
     float movementSpeed = 0.1f;
     float rotationSpeed = 1;
-    Vector3 Achicarse, Spawn;
-    bool HasJumped;
+    Vector3 Achicarse, Spawn, ScaleFrio, ScaleCaliente;
+    bool HasJumped, Limite;
     Rigidbody rb;
+    public GameObject player;
 
     void Start()
     {
         Achicarse = new Vector3(-0.001f, -0.001f, -0.001f);
+        ScaleFrio = new Vector3(0.0075f, 0.0075f, 0.0075f);
+        ScaleCaliente = new Vector3(0.0075f, 0.0075f, 0.0075f);
         Spawn = new Vector3(-4, 0.5f, 0);
         rb = GetComponent<Rigidbody>();
     }
@@ -53,13 +57,19 @@ public class Player_Movement : MonoBehaviour
             transform.Rotate(0, -rotationSpeed, 0);
         }
 
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.position = Spawn;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && HasJumped)
         {
             rb.AddForce(Vector3.up * 7, ForceMode.Impulse);
             HasJumped = false;
         }
 
-        if(transform.position.y < -8)
+        if(transform.position.y < -11)
         {
             transform.position = Spawn;
         }
@@ -75,8 +85,37 @@ public class Player_Movement : MonoBehaviour
 
         if (col.gameObject.tag == "Death")
         {
-            transform.position = Spawn;
             transform.localScale = new Vector3(1, 1, 1);
+            transform.position = Spawn;
+        }
+
+        if (col.gameObject.tag == "Placa Fria")
+        {
+            if (transform.localScale.y < 3.0)
+            {
+                while (Limite)
+                {
+                    transform.localScale += ScaleFrio;
+                }
+            }
+
+            else
+            {
+                Limite = false;
+            }
+        }
+
+        if (col.gameObject.tag == "Placa Caliente")
+        {
+            if (transform.localScale.y > 0.2)
+            {
+                transform.localScale += ScaleCaliente;
+            }
+
+            else
+            {
+                transform.position = Spawn;
+            }
         }
     }
 }
